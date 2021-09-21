5c80f146ca0593fb790a503f72605aa870201107
Nicolas Heizenreder <heizenredernicolas@gmail.com>
2021-09-20 22:56:59 -0300

    Design fixed

--- src/components/EditPatientForm.js
+++ src/components/EditPatientForm.js
@@ -16,22 +16,25 @@ import Container from '@material-ui/core/Container';
 const useStyles = makeStyles((theme) => ({
     component:{
         display:"flex",
-        width:"100vw",
-        height:"100vh",
+        padding:"20px",
         backgroundColor: "#0d2235"
     },
     paper: {
-        marginTop: theme.spacing(8),
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
     },
     form: {
-        width: '100%',
         marginTop: theme.spacing(1),
+        backgroundColor: "#fff",
+        padding:"10px 20px 10px 20px",
+        borderRadius: "5px",
     },
     submit: {
         margin: theme.spacing(3, 0, 2),
+    },
+    title:{
+        color:"#fff",
     },
 }));
 
@@ -44,57 +47,77 @@ export const EditPatientForm = () => {
             <Container component="main" maxWidth="xs">
             <CssBaseline />
             <div className={classes.paper}>
-                <Typography component="h1" variant="h5">
-                Editar
+                <Typography className={classes.title} component="h1" variant="h5">
+                Editar paciente
                 </Typography>
                 <form className={classes.form} noValidate>
-                <TextField
-                    variant="outlined"
-                    margin="normal"
-                    required
-                    fullWidth
-                    id="email"
-                    label="Email Address"
-                    name="email"
-                    autoComplete="email"
-                    autoFocus
-                />
-                <TextField
-                    variant="outlined"
-                    margin="normal"
-                    required
-                    fullWidth
-                    name="password"
-                    label="Password"
-                    type="password"
-                    id="password"
-                    autoComplete="current-password"
-                />
-                <FormControlLabel
-                    control={<Checkbox value="remember" color="primary" />}
-                    label="Remember me"
-                />
-                <Button
-                    type="submit"
-                    fullWidth
-                    variant="contained"
-                    color="primary"
-                    className={classes.submit}
-                >
-                    Sign In
-                </Button>
-                <Grid container>
-                    <Grid item xs>
-                    <Link href="#" variant="body2">
-                        Forgot password?
-                    </Link>
-                    </Grid>
-                    <Grid item>
-                    <Link href="#" variant="body2">
-                        {"Don't have an account? Sign Up"}
-                    </Link>
-                    </Grid>
-                </Grid>
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="patient"
+                        label="Nombre del paciente"
+                        name="patient"
+                    />
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="dentist"
+                        label="Odontologo"
+                        type="text"
+                        id="dentist"
+                    />
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="numberOfPlates"
+                        label="Numero de placas"
+                        type="number"
+                        id="numberOfPlates"
+                    />
+                    <TextField
+                        id="startTreatment"
+                        label="Inicio del tratamiento"
+                        type="date"
+                        required
+                        name="startTreatment"
+                        margin="normal"
+                        fullWidth
+                        variant="outlined"
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                        fullWidth
+                    />
+                    <TextField
+                        id="finishTreatment"
+                        label="Fin del tratamiento"
+                        margin="normal"
+                        required
+                        type="date"
+                        name="finishTreatment"
+                        fullWidth
+                        variant="outlined"
+                        InputLabelProps={{
+                        shrink: true,
+                        }}
+                        fullWidth
+                    />
+
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                    >
+                        Guardar
+                    </Button>
                 </form>
             </div>
             </Container>
